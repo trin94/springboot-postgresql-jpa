f2574b75d79c70c01c95b62a8d070c4a8e96dc94
Elias Müller <elias.mr1@gmail.com>
2023-02-18 11:15:45 +0100

    🚚 Rename main class

--- src/main/java/com/example/sbplrest/MySpringBootApplication.java
+++ src/main/java/com/example/sbplrest/MySpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbplrestApplication {
+public class MySpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbplrestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
 
 }
